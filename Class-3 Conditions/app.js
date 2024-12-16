16da5a2fe40c4a077f0c13b8833b926db6c282a5
Muhammad Roman <mromankhan005@gmail.com>
2024-12-16 23:27:08 +0500

    Conditions

---
+++ Class-3 Conditions/app.js
@@ -0,0 +1,70 @@
+// let isEeligible = confirm("are you miraculate");
+// console.log(isEeligible);
+
+
+
+
+
+
+
+// let isMudassirShopOpen = confirm("is mudaasir shop open?");
+// if (isMudassirShopOpen) {
+//     console.log("purchase pulses");
+// }
+
+
+
+
+
+
+// let age = +prompt("what is your age");
+
+// if (age < 15) {
+//     console.log ("ypu are eligible for addmisssion.");
+// }
+
+
+
+
+
+
+
+
+
+
+// let isShopOpen = confirm('Is Mudassir shop openn?');
+// console.log(isShopOpen);
+
+
+
+
+
+// let isShopOpen = confirm("is mudassir shop open?");
+// if(isShopOpen){
+//     document.write("purchase pulses")
+// }
+
+// else{
+//     document.write("come back to home");
+// }
+
+
+
+
+
+// let isShopOpen = confirm("is mudassir shop open");
+// if(isShopOpen){
+//     document.write("purchase pulses");
+// }
+
+// else{
+//     document.write("purchase chicken");
+    
+// }
+
+
+
+
+
+
+
