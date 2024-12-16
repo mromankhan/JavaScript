b8a2d6052245af9446ad478e86d29b8c46e123d4
Muhammad Roman <mromankhan005@gmail.com>
2024-12-16 23:22:06 +0500

    main

---
+++ Class-2 Prompt/app.js
@@ -0,0 +1,85 @@
+// let userName = prompt("what is your name")
+// let number1 = +prompt("enter your lucky number");
+// let number2 = +prompt ("enter your 2nd number");
+// document.write('welcome' + userName);
+// document.write("</ br>")
+// document.write("your total is" + number1 + number2)
+
+
+
+
+
+
+// let num1 = 5;
+// let num2 = 7;
+// let sum = num1++ + ++num1 + num2++ - ++num2; 
+
+// document.write("your total is:" + (num1 + num2));
+
+
+
+// let userName = prompt("what is your name");
+// let num1 = +prompt("enter your number");
+// let num2 = +prompt("enter your second number");
+// document.write('hello! welcome sir ' + userName);
+// document.write("<br>");
+// document.write(' m.r ' + userName + " your total amount is " + num1 + num2);
+
+
+
+// let num1 = 5;
+// let num2 = 6;
+// let sum = num1++ + ++num1 + num2++ - ++num2 - ++num2;
+
+// document.write("your total is:" + (num1 + num2));
+
+
+let table = +prompt("which table you want");
+let count = 1;
+let total = table*count;
+document.write(table + " x " + count + ' = ' + total*count + "<br>");
+document.write(table + " x " + ++count + ' = ' + total*count + "<br>");
+document.write(table + " x " + ++count + ' = ' + total*count + "<br>");
+document.write(table + " x " + ++count + ' = ' + total*count + "<br>");
+document.write(table + " x " + ++count + ' = ' + total*count + "<br>");
+document.write(table + " x " + ++count + ' = ' + total*count + "<br>");
+document.write(table + " x " + ++count + ' = ' + total*count + "<br>");
+document.write(table + " x " + ++count + ' = ' + total*count + "<br>");
+document.write(table + " x " + ++count + ' = ' + total*count + "<br>");
+document.write(table + " x " + ++count + ' = ' + total*count + "<br>");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
