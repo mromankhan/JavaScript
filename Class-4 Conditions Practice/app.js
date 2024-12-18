560d62fddfcfe3a9952d0e5e40119add12c97b83
Muhammad Roman <mromankhan005@gmail.com>
2024-12-19 01:20:18 +0500

    Conditions Practice

---
+++ Class-4 Conditions Practice/app.js
@@ -0,0 +1,98 @@
+// let shopName = prompt('which shop is open?');
+// if (shopName === "farhaan") {
+//     console.log(
+//         'bri9ng french frise'
+//     );
+// }
+// else if (shopName === "almadina") {
+//     console.log("egg burger");
+// }
+
+// else if (shopName === "mairaaj"){
+//     console.log('purchase sweets');
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let isMiraculate = confirm("are you miraculate?");
+// let enteryTestScore = +prompt('WHAT WAS YOUR SCORE');
+// if (isMiraculate && enteryTestScore>= 60){
+//     document.write("congrat's you are enrolled in smit");
+// }
+
+
+
+
+
+
+// let foodAvailable = prompt("which food is available");
+// if(foodAvailable === "french fries")
+
+
+
+
+
+
+
+
+
+
+// let foodAvailable = prompt("which food is available?");
+// let rasturentName = prompt ("which resturent has teasty food?")
+
+// if(foodAvailable === "french fries" || 
+// foodAvailable === "burger"){
+//     console.log("bring 1");
+// }
+
+// else if (rasturentName === "")
+
+
+
+
+
+
+
+
+// let weight = +prompt("what is your weight");
+// let time = +prompt("in how much time you run 50 meters?");
+// if(weight <= '300 ponds' && time <= "6 seconds"){
+//     console.log("congrat's you are selected");
+// }
+
+
+
+// let sat = +prompt('what is your sat?');
+// let avg = +prompt('what is your avg?');
+// let sports = prompt("what is your sports?");
+// if(sat > avg || sports === "football"){
+//     console.log("welcome you are selected");
+// }
+
+
+
+
+
+
+
+
+// let weight = +prompt('what is your weight?');
+// let bodyFat = +prompt('what is your body fat?');
+// let sports = prompt('which sports do you play?');
+// if (weight > '300' && bodyFat <= "20" || sports === "cricket"){
+//     console.log("you are eligible for addmission!");
+// }
+
+
+
+
+
+
