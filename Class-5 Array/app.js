2deadee6584cf01df4bad09f44733fd04d26bedd
Muhammad Roman <mromankhan005@gmail.com>
2024-12-19 01:23:48 +0500

    Array

---
+++ Class-5 Array/app.js
@@ -0,0 +1,34 @@
+// let student = ["naveen", "mubashir", "tauha", "qitmeer", "akram"];
+// console.log(student [4],  student[3]) 
+
+
+
+
+// let car = ["corolla", "civic", "audi"];
+// console.log(car[0]);
+
+
+
+
+// let students = [];
+// students[0] = "haris";
+// students[2] = "noman";
+// console.log(students[0],students[2]);
+
+
+
+
+
+
+
+
+// let vovels = ['a','e','i','o','u'];
+// let chr = prompt('enter a character');
+// if (vovels.includes(chr)    ){
+// console.log("i is a vovel code wok!");
+// }
+// else {
+//     console.log("wrong")}
+
+
+
