8289e8af193924509367732f1505ae1c62d57ab4
Muhammad Roman <mromankhan005@gmail.com>
2024-12-21 00:10:09 +0500

    Array Methods

---
+++ Class-6 Array Methods/app.js
@@ -0,0 +1,153 @@
+// let books = ['bahhar e shriat','fatawa rizwiya','balaghat','maths'];
+// let userRequiredBbook = prompt("which book you have need?");
+// if (books.indexOf(userRequiredBbook)!== -1 ){
+// console.log('your required book index is ' + userRequiredBbook(bookIndex));
+// }
+
+
+// let fruits = ['apple','mango','banana','grapes'];
+// fruits.pop();
+// fruits.push("oranges");
+// fruits.shift()
+// fruits.unshift('melon')
+
+// console.log(fruits)
+
+
+// let fruits = ['apple','mango','banana','grapes'];
+// fruits.splice(2,1,"water melon");
+// console.log(fruits)
+
+
+
+
+// let num = [1,2,3,4,5,6,7,8,9];
+// let fav = num.slice(3, 8);
+// console.log(fav);
+
+
+
+
+// let cricters = ['afridi','misbah','dhooni','shoaib akhtar','muhammad asif','ali', 'wasif'];
+// let favCricketers = cricters.slice(2, 6);
+// console.log(favCricketers);
+
+
+
+
+// let strArr = ['r','o','m','a','n'];
+// console.log(strArr.reverse().join(""));
+
+
+
+// let arr = ['a','b','c','d','e','f',];
+// console.log(arr.reverse() .join("-") );
+
+
+
+// let firstName = prompt('enter your first name');
+// let secondName = prompt('enter your second name');
+// let thirdName = prompt('enter your third name');
+// let arry = [firstName];
+// arry.push(secondName,thirdName);
+// console.log(arry);
+
+
+
+
+
+// let firstName = "haider ali shah";
+// console.log(firstName.length );
+// console.log(firstName.slice(7));
+
+// let firstName = "Haider Ali Shah";
+// console.log(firstName.toUpperCase());
+
+// let userCity = "karachi";
+// let city = prompt('enter your city name');
+// if (city.toLowerCase() === userCity .toLowerCase()){
+//     console.log('you live in the cleenest city');
+// }
+// else {
+//     console.log('sorry');
+// }
+
+
+
+
+
+// let names = ['muhammad', 'roman', 'khan',];
+// let naames = names.join(" ");
+// console.log(naames);
+
+
+
+// let word = prompt("enter a word");
+// let find = word.reverse;
+// document.write(find);
+
+
+
+
+
+
+// for (let i = 0; i < 10; i++){
+//     console.log(i);
+// }
+// console.log("hello world");
+
+
+
+// for (let i = 100; i > 0; i--){
+//     console.log("current iteration:" + i);
+// }
+
+
+
+
+// for ( let i = 0; i < 100; i = i + 2){
+//     console.log("current iteration:" + i);
+// }
+
+
+
+
+
+
+
+// let table = +prompt ("enter table name");
+// for (let i = 1; i < 11; i++){
+//     console.log(table + " x " + i + " = " + table * i);
+// }
+
+
+
+
+
+
+// let students = ['aslam', 'akram', 'ali', 'abdullah'];
+// for (let i = 0; i < students.length; i++){
+//     console.log('hello ' + students[i])
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
