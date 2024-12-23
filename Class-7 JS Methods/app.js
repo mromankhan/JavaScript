57fe737f8e3f90904e2af5adc8d6ec110dd987ac
Muhammad Roman <mromankhan005@gmail.com>
2024-12-24 00:11:04 +0500

    JS Methods

---
+++ Class-7 JS Methods/app.js
@@ -0,0 +1,114 @@
+// let prise = "25";
+// console.log(age);
+
+// age = Number.parseInt();
+
+// console.log(age);
+
+
+
+// let price = "25";
+// console.log(isNaN(price));
+
+// let num = "200002.000088";
+// num = Number(num);
+// console.log(num);
+
+// let num = 23456;
+// num = num.toString();
+// console.log(num);
+
+// let bill = 2.548236;
+// console.log(bill.toFixed(6));
+
+
+
+
+
+
+
+
+// let price = "25.5666666343";
+// console.log(price);
+// price = parseFloat(price);
+// console.log(price);
+
+
+
+
+
+
+
+// let age = "26.9987565" + "44";
+// age = Number(age);
+// console.log(age);
+
+
+
+
+
+
+
+
+
+
+
+// let age = "apple";
+// age = isNaN();
+// console.log(age);
+
+
+
+
+
+
+// let num = 23456789;
+// num = num.toString()
+// console.log(num.split(""));
+
+
+
+
+
+
+
+
+
+// let bill = 2.456789876; 
+// bill = bill.toFixed(9);
+// console.log(bill);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
