a3bf86f6dd45cac73b31525c6fe1e6af4736079e
Muhammad Roman <mromankhan005@gmail.com>
2025-01-03 15:03:25 +0500

    For Loop

--- Class-8 For Loop/app.js
+++ Class-8 For Loop/app.js
@@ -1,1 +1,48 @@
-console.log("hello");
+// for(let i = 1; i <= 10; i++){
+//     console.log(i,"hello");
+// }
+
+
+
+
+
+
+
+// for(let i = 0; i < 5; i--){
+//     console.log("Current iteration:" + i);
+// }
+
+
+// for(let i = 0; i > 5; i++){
+//     console.log("Current iteration:" + i);
+// }
+
+
+
+
+
+
+
+
+
+
+
+// let table = prompt("which table do you want");
+// let tableNum = Number(table);
+
+// for(let i = 1; i <= 10; i++){
+//     console.log(`${tableNum} x ${i} = ${tableNum * i}`);
+// }
+
+
+
+
+
+
+
+// let students = ["aslam", 'akram', 'muzammil', 'abdullah'];
+
+// for(let i = 0; i < students.length; i++){
+//     console.log(`Hello ${students[i]}`);
+    
+// }
