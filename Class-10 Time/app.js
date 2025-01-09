fca292b215cd705771b3dbbf6e64854c42ccc5f7
Muhammad Roman <mromankhan005@gmail.com>
2025-01-09 22:03:42 +0500

    Date & Time

---
+++ Class-10 Time/app.js
@@ -0,0 +1,233 @@
+// let now = new Date("june 20 2008 5:30:45");
+// console.log(now.getTime());
+
+
+
+
+// let day = now;
+// console.log(day.getDay());
+
+
+
+
+// let dob = new Date();
+// console.log(dob.getTime(dob));
+
+
+
+
+// let day = new Date("june 30, 2035");
+// let today = new Date();
+// day = day.getTime();
+// today = today.getTime();
+
+// let remain = day - today;
+// console .log(remain /1000 / 60/ 60/24 /365);
+
+
+
+
+
+
+
+// let now = new Date();
+// console.log(now);
+
+
+
+
+
+
+// let day = now;
+// console.log(day.getDay());
+
+// let month = now;
+// console.log(month.getMonth())
+
+
+// let date = now;
+// console.log(date.getDate());
+
+
+// let year = now;
+// console.log(year.getFullYear());
+
+
+// let hours = now;
+// console.log(hours.getHours());
+
+
+// let minutes = now;
+// console.log(minutes.getMinutes());
+
+
+// let sec = now; 
+// console.log(sec.getSeconds());
+
+
+// let milisec = now;
+// console.log(milisec.getMilliseconds());
+
+// let time = now;
+// console.log(time.getTime());
+
+
+// let dayNames = ['monday','tuesday','wednesday','thursday','friday','saturday','sunday'];
+// let day = now.getDay();
+// console.log(day);
+// console.log(dayNames[day]);
+
+
+
+
+// let months = ['jan','feb','mar','apr','may','jun','july','aug','sep','oct','nov','dec'];
+// let mon = now.getMonth();
+// console.log(mon);
+// console.log(months[mon]);
+
+
+
+
+
+// let dob = new Date("june 20, 2008");
+// let now = new Date();
+
+// let age = now.getTime() - dob.getTime();
+// console.log(age / 1000 / 60 / 60 / 24 / 365);
+
+
+
+
+// let indep = new Date("august 14, 1947");
+// console.log(indep);
+
+
+
+
+
+
+// let startDay = new Date("december 13, 2023");
+// let today = new Date();
+// let diff = today - startDay;
+// console.log(diff / 1000 / 60 / 60 / 24 / 365);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
